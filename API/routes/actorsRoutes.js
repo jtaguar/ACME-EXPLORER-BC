ecf5812c4aafef4a2e1eb70083a4132db00fcee4
favsol26 <favsol26@gmail.com>
2019-02-15 21:43:01 +0100

    adding db

--- API/routes/actorsRoutes.js
+++ API/routes/actorsRoutes.js
@@ -1,36 +1,36 @@
 'use strict';
-module.exports=function(app){
-    
-    var actor= require ('../controllers/actorsCtrl');
-    
-    /**
-   
-    * Post an actor 
-    *    RequiredRoles: None
-    * Put an actor 
-    *    RequiredRoles: None
- 
-	 * @section actors
-	 * @type post put
-	 * @url /v1/actors
-   * @param {string} role (explorer|administrator|manager) 
-   */
-    
-   app.route('/v1/actors')
-    .post(actors.create_an_actor);
-    
+module.exports = function (app) {
 
-      /**
-   * Put an actor
-   *    RequiredRoles: to be the proper actor
-	 * @section actors
-	 * @type  put
-	 * @url /v1/actors/:DNI
-  */  
+  var actor = require('../controllers/actorsCtrl');
+
+  /**
+ 
+  * Post an actor 
+  *    RequiredRoles: None
+  * Put an actor 
+  *    RequiredRoles: None
+ 
+ * @section actors
+ * @type post put
+ * @url /v1/actors
+ * @param {string} role (explorer|administrator|manager) 
+ */
+
+  app.route('/v1/actors')
+    .post(actors.create_an_actor);
+
+
+  /**
+* Put an actor
+*    RequiredRoles: to be the proper actor
+* @section actors
+* @type  put
+* @url /v1/actors/:DNI
+*/
   app.route('/v1/actors/:DNI')
-  .put(actors.update_an_actor)
-  
-    
+    .put(actors.update_an_actor)
+
+
 
 
 }
--- API/routes/tripsRoutes.js
+++ API/routes/tripsRoutes.js
@@ -1,8 +1,8 @@
 'use strict';
-module.exports=function(app){
-    
-    var application= require ('../controllers/tripsCtrl');
-   
+module.exports = function (app) {
+
+    var application = require('../controllers/tripsCtrl');
+
     /**
    
     * Post an trip
@@ -15,25 +15,22 @@ module.exports=function(app){
    * @url /v1/applications
    * @param {string} sortedBy (category)
    */
+
+    app.route('/v1/applications')
+        .post(application.create_an_trip)
+        .get(application.search_trip);
+
+    /**
+     * Put an applications
+     * Delete an applications
     
-  app.route('/v1/applications')
-  .post(application.create_an_trip)
-  .get(application.search_trip);
-
-  /**
-   * Put an applications
-   * Delete an applications
-  
-   * @section applications
-   * @type  put delete
-   * @url /v1/application/:ticker
-  */  
- app.route('/v1/trips/:ticker')
- .put(application.update_an_trip)
- .delete(application.delete_an_trip);
- 
-
+     * @section applications
+     * @type  put delete
+     * @url /v1/application/:ticker
+    */
+    app.route('/v1/trips/:ticker')
+        .put(application.update_an_trip)
+        .delete(application.delete_an_trip);
 
 }
 
-   
