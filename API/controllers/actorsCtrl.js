74ca331f9365a385a20f013d4c66c86eaa8b5718
favsol26 <favsol26@gmail.com>
2019-02-16 18:54:14 +0100

    fixing posts and devps

--- API/controllers/actorsCtrl.js
+++ API/controllers/actorsCtrl.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose'),
   Actor = mongoose.model('Actors');
 
 exports.create_an_actor = function (req, res) {
+  console.log((req.body));
   var new_actor = new Actor(req.body);
   new_actor.save(function (err, actor) {
     if (err) {
@@ -12,7 +13,6 @@ exports.create_an_actor = function (req, res) {
     else {
       res.json(actor);
     }
-    console.log('pass');
   });
 };
 
@@ -21,6 +21,7 @@ exports.update_an_actor = function (req, res) {
   Check that the user is the proper actor and if not: res.status(403);
    "an access token is valid, but requires more privileges"
   */
+  console.log((req.body));
   Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, { new: true }, function (err, actor) {
     if (err) {
       res.send(err);
--- routerSevices.js
+++ routerSevices.js
@@ -1,9 +1,14 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
     app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var routesActors = require('./API/routes/actorsRoutes');
 var routesApplications = require('./API/routes/applicationsRoutes');
 var routesTrips = require('./API/routes/tripsRoutes');
-
 
 module.exports = app, routesActors(app), routesApplications(app), routesTrips(app);
