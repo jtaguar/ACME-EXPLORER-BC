2035496c0cbc7c7504cbfc823589f392ecd8fe83
Chanell13 <45261718+Chanell13@users.noreply.github.com>
2019-03-23 16:01:16 +0100

    change position status

--- API/model/tripsModel.js
+++ API/model/tripsModel.js
@@ -32,10 +32,10 @@ var TripSchema = new Schema({
         ref: 'Actor',
         required: 'Kindly enter a valid manager of trip'
     },
-    published: {
+  /*  published: {
         type: Boolean,
         default: false
-    },
+    },*/
     ticker: {
     //    This validation does not run after middleware pre-save 
     //    required: 'Kindly enter the ticker of the Trip',
@@ -68,6 +68,11 @@ var TripSchema = new Schema({
     list_requirements: {
         type: [String] //['adios','hola']
     },
+    status: {
+        type: String,
+        enum: ['CREATED', 'PUBLISHED', 'STARTED', 'ENDED', 'CANCELLED'],
+        default: 'CREATED'
+    },
     date_start: {
         type: Date,
         required: 'Kindly enter the start of the Trip'
@@ -89,12 +94,7 @@ var TripSchema = new Schema({
     created: {
         type: Date,
         default: Date.now
-    },
-    status: {
-        type: String,
-        enum: ['CREATED', 'PUBLISHED', 'STARTED', 'ENDED', 'CANCELLED'],
-        default: 'CREATED'
-    },
+    }
 
 }, { strict: false });
 
